414e4f98841c9f727ec31fcd4b25e7e70a165b5e
thiroyoshi <thiroyoshi@gmail.com>
2016-02-14 00:00:27 +0900

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,168 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', [
+  'ionic',
+  'starter.controllers',
+  'starter.services',
+  'starter.directives.star-rating',
+  'starter.directives.comments',
+  'starter.directives.pokete-image',
+  'starter.directives.pokete-image-add',
+  'ionic-datepicker'
+])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  .state('tab.poketes', {
+    url: '/poketes',
+    views: {
+      'tab-poketes': {
+        templateUrl: 'templates/tab-poketes.html',
+        controller: 'PoketesCtrl'
+      }
+    },
+    resolve: {
+      poketes: function(Pokete) {
+        return Pokete.all();
+      }
+    }
+  })
+  .state('tab.addpokete', {
+    url: '/addpokete',
+    views: {
+      'tab-addpokete': {
+        templateUrl: 'templates/tab-addpokete.html',
+        controller: 'AddPoketeCtrl'
+      }
+    }
+  })
+
+/*
+  .state('tab.stock-detail', {
+    url: '/stocks/:id',
+    views: {
+      'tab-trends': {
+        templateUrl: 'templates/tab-stock-detail.html',
+        controller: 'StockDetailCtrl'
+      }
+    },
+    resolve: {
+      trend: function($stateParams, Trend) {
+        return Trend.get($stateParams.id);
+      }
+    }
+  })
+  */
+/*
+  .state('tab.news', {
+    url: '/news',
+    views: {
+      'tab-news': {
+        templateUrl: 'templates/tab-news.html',
+        controller: 'NewsCtrl'
+      }
+    }
+  })
+*/
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  })
+
+  .state('tab.qrread', {
+    url: '/qrread',
+    views: {
+      'tab-qrread': {
+        templateUrl: 'templates/tab-qrread.html',
+        controller: 'QrreadCtrl'
+      }
+    }
+  })
+
+  .state('tab.qrread_sample', {
+    url: '/qrread_sample',
+    views: {
+      'tab-qrread': {
+        templateUrl: 'templates/tab-qrread_sample.html',
+        controller: 'QrreadSampleCtrl'
+      }
+    },
+    resolve: {
+      poketes: function(Pokete) {
+        return Pokete.all();
+      }
+    }
+  })
+
+  .state('tab.manage-account', {
+    url: '/manage-account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-manage-account.html',
+        controller: 'ManageAccountCtrl'
+      }
+    }
+  })
+  .state('tab.manage-password', {
+    url: '/manage-password',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-manage-password.html',
+        controller: 'ManagePasswordCtrl'
+      }
+    }
+  })
+  .state('tab.manage-sign', {
+    url: '/manage-sign',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-manage-sign.html',
+        controller: 'ManageSignCtrl'
+      }
+    }
+  })
+
+  //
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/poketes');
+
+});
---
+++ www/js/services.js
@@ -0,0 +1,86 @@
+angular.module('starter.services', [])
+.factory('Pokete', function() {
+
+  var poketes = [{
+    id: "0102345",
+    price: "10,000",
+    password: "pass",
+    date: "2016/01/23",
+    limit: "2016/03/31"
+  }, {
+    id: "0102449",
+    price: "20,000",
+    password: "pass",
+    date: "2016/01/24",
+    limit: "2016/02/14"
+  }, {
+    id: "0104410",
+    price: "30,000",
+    password: "pass",
+    date: "2016/01/26",
+    limit: "2016/05/01"
+  }];
+
+  return {
+    all: function() {
+      return poketes;
+    },
+    get: function(id) {
+      return poketes.filter(function(pokete) { return pokete.id + '' === id; })[0];
+    }
+  };
+})
+
+
+
+/*
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+*/
+;
